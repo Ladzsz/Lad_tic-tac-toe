7ad04db5b24861b2987023298c6eb3b82d02933b
Ladzsz <londonmason602@gail.com>
2024-07-19 09:32:35 -0400

    added function to determine winner

--- script.js
+++ script.js
@@ -34,6 +34,24 @@ function Game() {
             gameboard[position] = symbol;
         } 
     }
+
+    //function to determine winner 
+
+    function checkWinner(gameBoard) {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  
+            [0, 4, 8], [2, 4, 6]              
+        ];
+        
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                return gameBoard[a]; 
+            }
+        }
+        return null; 
+    }
 }
 
 Game();
