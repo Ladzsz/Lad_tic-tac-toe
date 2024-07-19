1fb9446c76bcf0b1a030eb15416b2339b8204ca3
Ladzsz <londonmason602@gail.com>
2024-07-19 11:09:01 -0400

    added displayhing player to event

--- script.js
+++ script.js
@@ -17,18 +17,36 @@ function Game() {
 
         document.querySelector(".name_input").addEventListener("submit", function(event){
             event.preventDefault();
-    
+            
+            //making new player objects
             const player1 = document.getElementById("player1").value;
             const player2 = document.getElementById("player2").value;
 
             const P1 = new Player(player1, "X");
             const P2 = new Player(player2, "O");
 
+
+            //resetting form 
             event.target.reset();
 
             const inputArea = document.querySelector(".name_input");
             inputArea.style.display = "none";
 
+            //displaying player names and styling
+            const currentp1 = document.querySelector(".player1");
+            const currentp2 = document.querySelector(".player2");
+            
+            currentp1.style.fontWeight = 'bold';
+            currentp1.style.fontSize = '1.3em';
+
+            currentp2.style.fontWeight = 'bold';
+            currentp2.style.fontSize = '1.3em';
+
+            currentp1.innerHTML = `Player 1: ${P1.name}`;
+            currentp2.innerHTML = `Player 2: ${P2.name}`;
+
+
+            //calling render
             rendergame();
         });
     
@@ -36,8 +54,11 @@ function Game() {
     //function to render gameboard
 
     function rendergame() {
+
+        //grabbing board
         let board = document.querySelector(".game_display");
 
+        //creating cells
         gameboard.forEach(item => {
             
             if (item === "") {
@@ -60,12 +81,14 @@ function Game() {
     //function to determine winner 
 
     function checkWinner(gameBoard) {
+        //creating winning combos
         const winningCombinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],  
             [0, 3, 6], [1, 4, 7], [2, 5, 8],  
             [0, 4, 8], [2, 4, 6]              
         ];
         
+        //creating win logic
         for (let i = 0; i < winningCombinations.length; i++) {
             const [a, b, c] = winningCombinations[i];
             if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
@@ -78,6 +101,7 @@ function Game() {
     //function to display winner
 
     function displayWinner() {
+        // displaying the winner
         const winner = checkWinner(gameboard);
         if (winner) {
         let results = document.getElementsByClassName("results_display");
@@ -86,4 +110,5 @@ function Game() {
 }
 }
 
+//calling game
 Game();
